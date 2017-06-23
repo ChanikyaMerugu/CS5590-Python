360e0019e575d5824a605fdf94cefc96aa44f48f
ChanikyaMerugu <chanu.7894@gmail.com>
2017-06-22 22:51:13 -0500

    Update AllLetters.py

--- Assignment-2/Lab2/AllLetters.py
+++ Assignment-2/Lab2/AllLetters.py
@@ -7,14 +7,5 @@ k=set(str)                                             #assigning all values in 
 if k>=a :                                              #checking condition
   print('true')
 else:
+   print('false')
 =======
-import string                                           #importing string
-a=set(string.ascii_lowercase)                          #Assigning all lower case values to 'a'
-str= input('enter string')                             #input from user
-k=set(str)                                             #assigning all values in string from user to k
-
-if k>=a :                                              #checking condition
-  print('true')
-else:
->>>>>>> 6c926a1c43c3b530d8567d9d7892aaee56d27cc6
-  print('false')                                      #printing output
